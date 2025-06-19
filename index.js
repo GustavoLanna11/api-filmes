dd22ed922ecd2495dab08c827508b3a3b9290e09
unknown <gustavomlanna@gmail.com>
2025-06-19 19:23:09 -0300

    :sparkles: feat: conexão ao banco de dados

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 import express from "express";
 const app = express();
 
+// Importação do mongoose
+import mongoose from "mongoose"
+// iniciando a conexão com o banco de dados do MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/api-filmes");
+
+// Importação do Model de Filmes
+import Filme from "./models/Filmes.js";
+
 // Configuração do Express
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
