985f682da9142e0eef860e5a2f2f33d98bd0a00d
unknown <gustavomlanna@gmail.com>
2025-06-19 20:00:32 -0300

    :wrench: adição da importação do arquivo de rotas

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
 // Importação do Express
 import express from "express";
 const app = express();
-
 // Importação do mongoose
 import mongoose from "mongoose"
 // iniciando a conexão com o banco de dados do MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/api-filmes");
-
 // Importação do Model de Filmes
 import Filme from "./models/Filmes.js";
+
+import filmeRoutes from "./routes/filmeRoutes.js";
 
 // Configuração do Express
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
-// Dados para testar a API
-app.get("/", (req,res) => {
-    const filmes = [
-        {
-            title: "Titanic",
-            year: "1909",
-            genre: "Romance",
-            ageRating: "12"
-        }, 
-        {
-            title: "Teste",
-            year: "2025",
-            genre: "Drama",
-            ageRating: "18"
-        }
-    ];
-    res.json(filmes);
-})
+app.use("/", filmeRoutes)
 
 // Rodando a API
 const port = 4000;
