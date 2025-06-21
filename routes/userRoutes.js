d351b4fb40e5b7f8cc67bc7e00787e776495ed7b
unknown <gustavomlanna@gmail.com>
2025-06-21 20:09:33 -0300

    :sparkles: feat: adição do endpoint para login do usuário + geração de token

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,7 @@ import userController from '../controllers/userController.js';
 
 // endpoint para cadastro de um usuário
 userRoutes.post("/user", userController.createUser)
+// endpoint para login do usuário
+userRoutes.post("/auth", userController.LoginUser)
 
 export default userRoutes
