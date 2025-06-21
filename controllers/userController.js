65c1bd8ed013d055976542a44a48dfbfd1c1235e
unknown <gustavomlanna@gmail.com>
2025-06-21 19:47:10 -0300

    :sparkles: feat: adição do método de login + geração do token

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,4 +14,51 @@ const createUser = async (req, res) => {
     }
 };
 
-export default { createUser }
+// Login do usuário + token de autenticação
+const LoginUser = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+        // email valido
+        if(email != undefined){
+            const user = await userService.getOne(email);
+            // usuario encontrado
+            if(user != undefined){
+                // senha incorreta
+                if(user.password == password){
+                    // cria o token e determina e seu tempo de validade
+                    jwt.sign(
+                        {id: user._id, email: user.email},
+                        JWTSecret,
+                        {expiresIn: "48h"},
+                        (err, token) => {
+                            if(err) {
+                                res.status(400);
+                                res.json({err: "Fala interna"});
+                            } else {
+                                res.status(200);
+                                res.json({token: token})
+                            }
+                        }
+                    );
+                    // senha incorreta
+                } else {
+                    res.status(401);
+                    res.json({err: "Credenciais Inválidas!"})
+                }
+                // Usuário não encontrado
+            } else {
+                res.status(404);
+                res.json({err: "o email enviado não foi encontrado!"})
+            }
+            // email inválido
+        } else {
+            res.status(400);
+            res.json({err: "o email enviado é inválido!"});
+        }
+    } catch (error ){
+        console.log(error);
+        res.sendStatus(500);
+    }
+};
+
+export default { createUser, LoginUser };
