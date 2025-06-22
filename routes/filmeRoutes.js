573fbd045eba24a57f682174c5cea70c3fcbdf94
unknown <gustavomlanna@gmail.com>
2025-06-21 22:10:58 -0300

    :wrench: fix: adição da autenticação por JWT nas rotas de filme

--- routes/filmeRoutes.js
+++ routes/filmeRoutes.js
@@ -1,17 +1,18 @@
 
-import express from 'express'
-const filmeRoutes = express.Router()
-import filmeController from '../controllers/filmeController.js'
+import express from 'express';
+const filmeRoutes = express.Router();
+import filmeController from '../controllers/filmeController.js';
+import Auth from '../middleware/Auth.js';
 
 // endpoint para listar os filmes
-filmeRoutes.get("/filmes", filmeController.getAllFilmes);
+filmeRoutes.get("/filmes", Auth.Authorization, filmeController.getAllFilmes);
 // endpoint para cadastrar um filme novo
-filmeRoutes.post("/filme", filmeController.createFilme);
+filmeRoutes.post("/filme", Auth.Authorization, filmeController.createFilme);
 // endpoint para deletar um filme
-filmeRoutes.delete("/filme/:id", filmeController.deleteFilme);
+filmeRoutes.delete("/filme/:id", Auth.Authorization, filmeController.deleteFilme);
 // endpoint para editar os filmes
-filmeRoutes.put("/filme/:id", filmeController.updateFilme);
+filmeRoutes.put("/filme/:id", Auth.Authorization, filmeController.updateFilme);
 // endpoint para lisatr um filme só
-filmeRoutes.get("/filme/:id", filmeController.getOneFilme);
+filmeRoutes.get("/filme/:id", Auth.Authorization, filmeController.getOneFilme);
 
 export default filmeRoutes
